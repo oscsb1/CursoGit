437e786bdd70f33518fdca3336a515032d76b8ac
oscar <oscsb1@gmail.com>
2020-02-13 17:48:05 -0300

    fase inicial

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class CalculaT
+    {
+
+        private int _ai;
+        public int A;
+        public int MyProperty { get; set; }
+        public int B;
+        public int CC
+        {
+            get { return _ai; }
+            set { _ai = value; }
+        }
+
+        public int ParamV(params int[] p1)
+        {
+            int j = 0;
+
+            foreach (int obj in p1)
+            {
+                j += obj;
+            }
+            for (int i = 0; i < p1.Length; i++)
+
+            {
+                j += p1[i];
+            }
+
+            return j;
+
+        }
+
+        public CalculaT(int ai, int a, int b)
+        {
+            this._ai = ai;
+            A = a;
+            B = b;
+        }
+
+        public CalculaT()
+        {
+        }
+
+        public int CalcArea()
+        {
+
+
+            return A * B + _ai;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            return obj is CalculaT t &&
+                   _ai == t._ai &&
+                   A == t.A &&
+                   B == t.B;
+        }
+    }
+}
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime d1 = DateTime.ParseExact("2020/02/13 ", "yyyy/MM/dd ", CultureInfo.InvariantCulture);
+
+            TimeSpan t1;
+                  
+            t1 = d1.Subtract(DateTime.Now);
+            Console.WriteLine(t1.TotalHours);
+
+
+
+
+
+
+
+        }
+    }
+}
